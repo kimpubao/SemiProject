b2c842c0569db931245fd6cc8a67b3144b853ff1
cleamm <ehdgus275@naver.com>
2024-05-16 14:33:52 +0900

    네비게이션 추가(홈버튼 기능 활성화, 로그인은 안됨)

--- pybo/urls.py
+++ pybo/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('pybo/', views.index, name='pybo'),
     path('search/', views.search, name='search'),
     path('<int:news_id>/', views.detail),
     path('summary/<int:news_id>/<str:content>/', views.summary, name='summary'),
