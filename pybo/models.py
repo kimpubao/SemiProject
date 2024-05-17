3c70c6b010e2c8d1a472c3037061480f7a1cb0b9
cleamm <ehdgus275@naver.com>
2024-05-17 16:12:10 +0900

    테이블 수정

--- pybo/models.py
+++ pybo/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class News(models.Model):
     subject = models.CharField(max_length=200)
-    content = models.TextField()
-    create_date = models.DateTimeField()
-    # news_date = models.DateTimeField()
-    link = models.TextField()
+    content = models.TextField(null=True)
+    create_date = models.DateTimeField(null=True)
+    news_date = models.DateTimeField(null=True)
+    link = models.TextField(null=True)
     summary = models.TextField(null=True, blank=True)
 
     def __str__(self):
